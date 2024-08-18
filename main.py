3322f621eb560d3146cf0c39859fdc56132b2028
claytonm17 <133607487+claytonm17@users.noreply.github.com>
2024-08-18 14:53:47 -0400

    Create metadata csv using pandas

--- main.py
+++ main.py
@@ -4,6 +4,29 @@ path = "./Library_8_18_2024.xml"
 
 library = Library(path)
 
-for song in library.parsexml():
+library.parsexml_to_df()
 
-    print(f'Song: {song.name}, Artist: {song.artist}')
+def artist_count(library):
+    # Argument is library object
+    count = {}
+
+    for song in library.metadata:
+
+        if song.artist in count.keys():
+
+            count[song.artist] += 1
+
+        else:
+
+            count[song.artist] = 1
+
+    return count
+
+# Export metadata to csv
+import pandas as pd
+
+metadata = library.metadata
+
+df = pd.DataFrame(metadata)
+
+df.to_csv("metadata.csv")
