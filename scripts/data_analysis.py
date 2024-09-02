e0b1354a1656c073d72ffb4ff426c2ad3c2390dd
claytonm17 <133607487+claytonm17@users.noreply.github.com>
2024-09-02 14:12:16 -0400

    Composition of library function built

--- scripts/data_analysis.py
+++ scripts/data_analysis.py
@@ -1,5 +1,38 @@
 import pandas as pd
 
-df = pd.read_csv("../metadata.csv")
+class Analysis:
 
-print(df["artist"])
+    def __init__(self):
+        self.data = pd.read_csv("../metadata.csv")
+
+    def composition_of_library(self):
+        
+        comp = {}
+        total = 0
+
+        for artist in self.data["album_artist"]:
+
+            if artist not in comp.keys():
+                comp[artist] = 1
+            
+            else:
+                comp[artist] += 1
+
+            total += 1
+        
+        for artist,songs in comp.items():
+
+            print(f'{artist}: {round(((songs/total) * 100), 2)}%')
+
+    def find_missing_artist(self):
+
+        for artist in self.data["album_artist"]:
+
+            if artist == "NaN":
+
+                print(artist)
+
+
+analytics = Analysis()
+#print(analytics.composition_of_library())
+print(analytics.find_missing_artist())
