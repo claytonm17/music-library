8e9f6bd471940ed768098fc3413b258aa7daebb6
claytonm17 <133607487+claytonm17@users.noreply.github.com>
2024-09-03 20:32:47 -0400

    Refactor to use built in data types

--- scripts/data_analysis.py
+++ scripts/data_analysis.py
@@ -1,22 +1,24 @@
-import pandas as pd
+from library import Library
 
 class Analysis:
 
-    def __init__(self):
-        self.data = pd.read_csv("../metadata.csv")
+    def __init__(self, metadata):
+        self.metadata = metadata
 
     def composition_of_library(self):
         
         comp = {}
         total = 0
 
-        for artist in self.data["album_artist"]:
+        for song in self.metadata:
 
-            if artist not in comp.keys():
-                comp[artist] = 1
+            album_artist = song['album_artist']
+
+            if album_artist not in comp:
+                comp[album_artist] = 1
             
             else:
-                comp[artist] += 1
+                comp[album_artist] += 1
 
             total += 1
         
@@ -24,15 +26,16 @@ class Analysis:
 
             print(f'{artist}: {round(((songs/total) * 100), 2)}%')
 
-    def find_missing_artist(self):
+lib = Library("../Library.xml")
+lib.parsexml()
+metadata = lib.metadata
 
-        for artist in self.data["album_artist"]:
+analytics = Analysis(metadata)
+'''
+for data in analytics.metadata:
 
-            if artist == "NaN":
+    print(data['artist'])
 
-                print(artist)
-
-
-analytics = Analysis()
-#print(analytics.composition_of_library())
-print(analytics.find_missing_artist())
+print(len(analytics.metadata))
+'''
+analytics.composition_of_library()
